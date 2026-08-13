241002d4715f05ffaa648cccde99d64475891694
samgirma <girmasamuel200@gmail.com>
2026-08-13 07:55:56 +0300

    Update docs URL to doc.broslaptop.com

--- admin-portal/src/constants/docLinks.js
+++ admin-portal/src/constants/docLinks.js
@@ -3,7 +3,7 @@
 
 export const DOC_LINKS = {
   // Developer documentation (MkDocs Material site)
-  DEVELOPER_DOCS: 'https://docs.broslaptop.com',
+  DEVELOPER_DOCS: 'https://doc.broslaptop.com',
 
   // Client handover document (PDF stored in Supabase Storage)
   HANDOVER_PDF: 'https://xocojadwlmpzovxaeqvo.supabase.co/storage/v1/object/sign/hand%20over%20doc/BROS_Technology_Handover_Updated.pdf?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV9jY2IxMzU1NS05NDBiLTQ1NDctYTM4OC05NTU5YTE4YWVlYzEiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJoYW5kIG92ZXIgZG9jL0JST1NfVGVjaG5vbG9neV9IYW5kb3Zlcl9VcGRhdGVkLnBkZiIsInNjb3BlIjoiZG93bmxvYWQiLCJpYXQiOjE3ODY1OTE5MjYsImV4cCI6MTk0NDI3MTkyNn0.3pJ-v8zG6FizJREyTD-n9owYYxgUbpPY8zN1xaSavlE',
